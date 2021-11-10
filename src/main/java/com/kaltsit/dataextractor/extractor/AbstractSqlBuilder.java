38e81b1bd5fc93724083ee9118fa89d4ce90dbc1
wangcy <wangcy@goisan.cn>
2021-11-10 17:15:05 +0800

    代码重构

--- src/main/java/com/kaltsit/dataextractor/extractor/AbstractSqlBuilder.java
+++ src/main/java/com/kaltsit/dataextractor/extractor/AbstractSqlBuilder.java
@@ -1,25 +1,35 @@
-package com.kaltsit.util;
+package com.kaltsit.dataextractor.extractor;
 
-import com.kaltsit.converter.Converter;
+import com.kaltsit.dataextractor.util.StringUtils;
 
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author wangcy
- * @date 2021/9/26 9:59
+ * @date 2021/11/10 16:50
  */
-public class SqlBuilder {
+public abstract class AbstractSqlBuilder implements SqlBuilder {
 
-    public static String select(String table, String[] columns, String where) {
+    String table;
+    String[] columns;
+    String where;
+
+    protected AbstractSqlBuilder(String table, String[] columns, String where) {
+        this.table = table;
+        this.columns = columns;
+        this.where = where;
+    }
+
+    protected String select(String table, String[] columns, String where) {
         String sql = String.format("select %s from %s", StringUtils.join(columns, ","), table);
-        if(!StringUtils.isEmpty(where)) {
+        if(StringUtils.isNotEmpty(where)) {
             sql += " where " + where;
         }
         return sql;
     }
 
-    public static String insert(String table, String[] columns, Map<String, Object> map, List<Converter> converters) {
+    protected String insert(String table, String[] columns, Map<String, Object> map, List<ColumnValueConverter> converters) {
         String[] values = new String[columns.length];
         for(int i=0,len=columns.length; i<len; i++) {
             String column = columns[i];
@@ -28,8 +38,8 @@ public class SqlBuilder {
                 values[i] = null;
             }else {
                 values[i] = value.toString();
-                for(Converter converter : converters) {
-                    if(converter.access(column)) {
+                for(ColumnValueConverter converter : converters) {
+                    if(converter.accept(column)) {
                         values[i] = converter.process(value.toString());
                         break;
                     }
